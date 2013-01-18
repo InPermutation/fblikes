5b6ab324af095097fb127565dd2896940806a70a
Jacob Krall <jacob@jkrall.net>
2013-01-18 12:14:22 -0500

    fix default title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
     def app_id
         ENV['APP_ID']
     end
+
+    def title_for(title)
+        if(title.empty?)
+            "Facebook Likes"
+        else
+            title
+        end
+    end
 end
