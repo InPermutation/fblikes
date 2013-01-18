5b8ca02193a5cac6874b6a12012251026f08f0a3
Jacob Krall <jacob@jkrall.net>
2013-01-18 12:08:01 -0500

    Move FB graph API calls out of the views

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,9 +5,24 @@ class FriendsController < ApplicationController
   before_filter :api
   
   def index
+    @friends = @api.get_connections("me", "friends")
   end
 
   def get
+    @them = @api.get_object(params[:id])
+   
+    mine, theirs = @api.batch do |batch_api|
+        batch_api.get_connections("me", "likes")
+        batch_api.get_connections(@them['id'], 'likes')
+    end
+
+    mine.map! { |like| like['id'] }
+    theirs.map! { |like| like['id'] }
+
+    @shared = mine.select { |id| theirs.include? id }
+    @shared.map! { |id| @api.get_object(id) }
+
+
   end
 
   private
@@ -17,6 +32,7 @@ class FriendsController < ApplicationController
 
     def api
       @api = Koala::Facebook::API.new(session[:token])
+      @me = @api.get_object("me")
     rescue
       redirect_to root_url
     end
